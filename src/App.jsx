c215133d9a5e4d0caf3950a43dc1c04e7dd518b3
Reinaldo Romero <reinaldoromero696@gmail.com>
2023-11-30 21:10:10 -0400

    commit 1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
